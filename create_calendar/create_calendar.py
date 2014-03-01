64d462fb0b70b1245b30e6782df9c4cb125db63b
Mark Betnel <itmeson@gmail.com>
2014-02-28 16:35:10 -0800

    Added html stripping so summary can be output without tags. Simplified output to most basic ical format, using just dtstart and summary to specify the events.

--- create_calendar/create_calendar.py
+++ create_calendar/create_calendar.py
@@ -1,4 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Create Calendar
+===============
+
+This plugin parses the article metadata to find duedate: tags
+then compiles the relevant articles into entries in an icalendar file.
+"""
+
+
 from pelican import signals
+
 
 def test(sender):
     print "%s initialized !!" % sender
@@ -7,16 +18,57 @@ def find_due_dates(generator):
     assignments = []
     for article in generator.articles:
 	if hasattr(article,'duedate'): 
-	    print article.title, article.url, article.duedate
+	    print article.title, article.url, article.duedate, article.summary
 	    assignments.append(article)
-	else:
-	    print article.title, "no due date"
+	    q = article.summary
+	
     if len(assignments) > 0:
-	generate_ical(assignments)
+	generate_ical(assignments, generator.output_path)
 
-def generate_ical(assignments):
+def generate_ical(assignments, output_path):
+    from icalendar import Calendar, Event
+    import datetime
+    import pytz
+
+    cal = Calendar()
+ 
+    cal.add('prodid', '-//My calendar product//mxm.dk//')
+    cal.add('version', '2.0')
+
+    for a in assignments:
+        event = Event()
+	summary = strip_tags(a.summary).strip()
+        event.add('summary', summary)
+        date = datetime.datetime.strptime(a.duedate, '%Y-%m-%d')
+        event.add('dtstart', date)
+
+        cal.add_component(event)
+
+    import os
+    f = open(os.path.join(output_path, 'assignments.ics'), 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
     for a in assignments:
 	print a.title, a.url, a.duedate
+
+
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    """From stackoverflow: http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python"""
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 
 def register():
