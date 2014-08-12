edb1a259ab7254a3ee2e5d426e1b65a1c8ba79b9
Mark Betnel <itmeson@gmail.com>
2014-08-12 15:00:41 -0700

    Got create calendar to extract the siteurl, so it can work for multiple sites without modification.  The method of extracting the siteurl is probably fragile, but it works for now.

--- create_calendar/create_calendar.py
+++ create_calendar/create_calendar.py
@@ -12,36 +12,43 @@ from pelican import signals
 
 
 def test(sender):
-    print "%s initialized !!" % sender
+    #print "%s initialized !!" % sender
+    pass    
 
 def find_due_dates(generator):
+    import inspect
+    # there simply *has* to be a better way to get the siteurl, but this does work
+    a = inspect.getmembers(generator, lambda a:not(inspect.isroutine(a)))
+    SITEURL = a[2][1]['settings']['SITEURL']
+
     assignments = []
     for article in generator.articles:
 	if hasattr(article,'duedate'): 
-	    print article.title, article.url, article.duedate, article.summary
 	    assignments.append(article)
 	    q = article.summary
-	
     if len(assignments) > 0:
-	generate_ical(assignments, generator.output_path)
+	generate_ical(assignments, generator.output_path, SITEURL)
 
-def generate_ical(assignments, output_path):
+def generate_ical(assignments, output_path, SITEURL):
     from icalendar import Calendar, Event
     import datetime
     import pytz
-
-    cal = Calendar()
  
+    cal = Calendar()
+
     cal.add('prodid', '-//My calendar product//mxm.dk//')
     cal.add('version', '2.0')
 
-    for a in assignments:
+    for (count, a) in enumerate(assignments):
         event = Event()
 	summary = strip_tags(a.summary).strip()
         event.add('summary', summary)
         date = datetime.datetime.strptime(a.duedate, '%Y-%m-%d')
-        event.add('dtstart', date)
-
+	date += datetime.timedelta(hours=25)
+	description = 'More Info: ' + SITEURL + '/' + a.url + ' DUE: ' + a.duedate
+	event.add('dtstart', date)
+	event.add('uid', str(count) + 'mbetnel@seattleacademy.mrooms3.net')
+	event.add('description', description) 
         cal.add_component(event)
 
     import os
@@ -49,8 +56,6 @@ def generate_ical(assignments, output_path):
     f.write(cal.to_ical())
     f.close()
 
-    for a in assignments:
-	print a.title, a.url, a.duedate
 
 
 from HTMLParser import HTMLParser
